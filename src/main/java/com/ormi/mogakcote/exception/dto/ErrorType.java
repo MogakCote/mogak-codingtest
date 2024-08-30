0182aeb11accb485101030f19e6ac02f1d16a9a1
OhHaneol <otyvs1109@hs.ac.kr>
2024-08-30 10:39:52 +0900

    feat: add system comment logic

--- src/main/java/com/ormi/mogakcote/exception/dto/ErrorType.java
+++ src/main/java/com/ormi/mogakcote/exception/dto/ErrorType.java
@@ -33,7 +33,7 @@ public enum ErrorType {
     INVALID_AUTH_CHANGE_ERROR(HttpStatus.FORBIDDEN,"사용자의 권한을 변경 할 수 없습니다."),
     // notice 예외
     NOTICE_NOT_FOUND_ERROR(HttpStatus.NOT_FOUND, "공지사항을 찾을 수 없습니다."),
-  
+
     // user 예외
     USER_NOT_FOUND_ERROR(HttpStatus.NOT_FOUND, "유저를 찾을 수 없습니다"),
     SYSTEM_USER_NOT_FOUND_ERROR(HttpStatus.NOT_FOUND, "시스템 유저를 찾을 수 없습니다."),
