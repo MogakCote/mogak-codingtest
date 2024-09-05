3aeb0b823d683bb533a1cbc7c4da2b95a3931ae7
BaekEuiHun <euihunbaek@gmail.com>
2024-09-05 19:41:03 +0900

    회원정보 변경 로직 추가

--- src/main/java/com/ormi/mogakcote/user/application/UserService.java
+++ src/main/java/com/ormi/mogakcote/user/application/UserService.java
@@ -82,7 +82,8 @@ public class UserService {
     @Transactional
     public void updateProfile(Long userId, String username, String nickname) {
         User user = getById(userId);
-        user.updateProfile(username, nickname);
+        String email = "";
+        user.updateProfile(username,nickname,email);
     }
     @Transactional
     public void changePassword(Long userId, String currentPassword, String newPassword) {
