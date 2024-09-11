3ad57bf3b84d0821ad3eefa7d8c0242636068a7a
LeeJaejun98 <jj980302@naver.com>
2024-09-11 15:46:58 +0900

    fix : adminPage로 가는 get을 admin에 생성

--- src/main/java/com/ormi/mogakcote/admin/presentation/AdminController.java
+++ src/main/java/com/ormi/mogakcote/admin/presentation/AdminController.java
@@ -1,10 +1,29 @@
 package com.ormi.mogakcote.admin.presentation;
 
+import com.ormi.mogakcote.auth.model.AuthUser;
+import com.ormi.mogakcote.notice.application.NoticeService;
+import com.ormi.mogakcote.notice.dto.response.NoticeResponse;
 import com.ormi.mogakcote.post.application.PostService;
+import com.ormi.mogakcote.post.dto.request.PostSearchRequest;
 import com.ormi.mogakcote.post.dto.response.PostResponse;
+import com.ormi.mogakcote.post.dto.response.PostSearchResponse;
+import com.ormi.mogakcote.problem.application.AlgorithmService;
+import com.ormi.mogakcote.problem.application.LanguageService;
+import com.ormi.mogakcote.problem.application.PlatformService;
+import com.ormi.mogakcote.problem.domain.Platform;
+import com.ormi.mogakcote.problem.dto.response.AlgorithmResponse;
+import com.ormi.mogakcote.problem.dto.response.LanguageResponse;
+import com.ormi.mogakcote.problem.dto.response.PlatformResponse;
+import com.ormi.mogakcote.user.application.UserService;
+import com.ormi.mogakcote.user.dto.response.UserAuthResponse;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/admin")
@@ -12,6 +31,29 @@ import org.springframework.web.bind.annotation.*;
 public class AdminController {
 
     private final PostService postService;
+    private final NoticeService noticeService;
+    private final LanguageService languageService;
+    private final PlatformService platformService;
+    private final AlgorithmService algorithmService;
+    private final UserService userService;
+    @GetMapping
+    public ModelAndView adminPage(
+            Model model) {
+
+        List<UserAuthResponse> userAuthResponses = userService.getAll();
+        List<NoticeResponse> noticeResponses = noticeService.getNoticeList();
+        List<LanguageResponse> languageResponses = languageService.getLanguageList();
+        List<PlatformResponse> platformResponses = platformService.getPlatformList();
+        List<AlgorithmResponse> algorithmResponses = algorithmService.getAlgorithmList();
+
+        model.addAttribute("userList", userAuthResponses);
+        model.addAttribute("noticeTop5List", noticeResponses);
+        model.addAttribute("languageList", languageResponses);
+        model.addAttribute("platformList", platformResponses);
+        model.addAttribute("algorithmList", algorithmResponses);
+
+        return new ModelAndView("admin/adminPage");
+    }
     @PutMapping("/convertBanned/{postId}")
     public ResponseEntity<?> convertBanned(
             @PathVariable(name = "postId") Long id
@@ -19,4 +61,5 @@ public class AdminController {
         PostResponse updateBanned = postService.convertBanned(id);
         return ResponseEntity.ok(updateBanned);
     }
+
 }
